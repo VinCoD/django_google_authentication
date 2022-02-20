34647106ff4e71e7fee880318ef5cae59ef6b234
VinCoD <5895.2020@students.ku.ac.ke>
2022-02-20 22:59:42 +0300

    Initial commit

---
+++ socialauth/local_settings.py
@@ -0,0 +1,7 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-k6n)g0)kncoy0!(kcgkmlimar%!7(9(2wlvmjarto2%wn(g+$$'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
